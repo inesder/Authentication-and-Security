dc25392a348f47a634d9a8cb1a6f2471f3f8692b
Inès Derraz <inesderraz@hotmail.fr>
2022-07-23 14:02:47 +0200

    Level 2 encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose= require("mongoose")
+const encrypt= require ("mongoose-encryption");
 
 const app = express();
 
@@ -18,10 +19,14 @@ async function main() {
   await mongoose.connect('mongodb://localhost:27017/userDB');
 }
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+const secret = "thisisourlittlesecret.";
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
